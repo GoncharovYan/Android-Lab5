be32562240dc39775bf389baaa3ad2a1a4cf6bf7
Yan Goncharov <yan.goncharov1@mail.ru>
2021-10-28 18:29:29 +0200

    Android Lab5

---
+++ app/src/main/java/com/example/android_lab5/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.android_lab4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_lab4.databinding.ActivityMainBinding
+
+import com.example.android_lab4.fragments.EventsFragment
+import com.example.android_lab4.fragments.HolidayFragment
+import com.example.android_lab4.fragments.ProfileFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.BottomNavigation.setOnNavigationItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.BNEvents -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, EventsFragment())
+                        .commit()
+                    true
+                }
+                R.id.BNHolidays -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, HolidayFragment())
+                        .commit()
+                    true
+                }
+                R.id.BNProfile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, ProfileFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
